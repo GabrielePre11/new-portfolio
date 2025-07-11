27ef85a2d1e704ca6c21241689b927e0c8ae2934
Gabriele Prestano <gabryprestanoitaly@gmail.com>
2025-07-11 13:57:58 +0200

    Solved issue on Safari

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -46,7 +46,7 @@ export default function Projects() {
           ))}
         </ul>
 
-        <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
           {projects
             .filter((project) =>
               activeCategory === "Tutti"
